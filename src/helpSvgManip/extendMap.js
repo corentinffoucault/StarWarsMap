b511eefaafa68dec22635024041f3a21b7f2532e
corentin <c.foucault@bee-buzziness.com>
2025-01-16 11:18:03 +0100

    map extended

--- src/helpSvgManip/extendMap.js
+++ src/helpSvgManip/extendMap.js
@@ -3,54 +3,42 @@ const fs = require('fs/promises');
 
 var replacer = new Map();
 
-function updateSubGroup(group, parentMatrix) {
-    var reg = /translate\(([^\)]*)\)/g
-    var regRes = reg.exec(group['$'].transform)
-    var transformMatrix = parentMatrix
-    if (regRes) {
-        var matrix = regRes[1].split(',')
-        var translateMatrix = {
-            'a': 1,
-            'b': 0,
-            'c': 0,
-            'd': 1,
-            'e': +matrix[0],
-            'f': +matrix[1],
-        }
-        transformMatrix = multipliMatrix(translateMatrix, parentMatrix)
-    }
+var maxHeight = 0
+var maxWidth = 0
+
+function updateSubGroup(group, factor) {
     if (group.g && group.g.length > 0) {
         for (const g of group.g) {
-            updateSubGroup(g, transformMatrix)
+            updateSubGroup(g, factor)
         }
     }
     if (group.a && group.a.length > 0) {
         for (const a of group.a) {
-            updateSubGroup(a, transformMatrix)
+            updateSubGroup(a, factor)
         }
     }
 
     if (group.path && group.path.length > 0) {
         for (const path of group.path) {
-            path['$'].d = editPath(path['$'].d, transformMatrix)
+            path['$'].d = editPath(path['$'].d, factor)
         }
     }
 
     if (group.circle && group.circle.length > 0) {
         for (const circle of group.circle) {
-            circle['$'] = editFigure(circle['$'], transformMatrix)
+            circle['$'] = editFigure(circle['$'], factor)
         }
     }
 
     if (group.ellipse && group.ellipse.length > 0) {
         for (const ellipse of group.ellipse) {
-            ellipse['$'] = editFigure(ellipse['$'], transformMatrix)
+            ellipse['$'] = editFigure(ellipse['$'], factor)
             // modifier les transform
         }
     }
 }
 
-function editTransform(transform, currentMatrix) {
+function editTransform(transform, factor) {
     var reg = /rotate\(([^\)]*)\)/g;
     var regRes = reg.exec(transform)
     if (regRes) {
@@ -58,62 +46,52 @@ function editTransform(transform, currentMatrix) {
         if (rotate.length === 1) {
             return `rotate(${rotate[0]})`
         }
-        x = rotate[1]
-        y = rotate[2]
-        newX = x * currentMatrix['a'] - y * currentMatrix['b'] + currentMatrix['e']
-        newY = -x * currentMatrix['c'] + y * currentMatrix['d'] + currentMatrix['f']
+        newX = rotate[1] * factor
+        newY = rotate[2] * factor
+        maxHeight = Math.max(newY, maxHeight)
+        maxWidth = Math.max(newX, maxWidth)
         return `rotate(${rotate[0]}, ${newX}, ${newY})`
     }
     return transform
 }
 
-function editFigure(currentFigure, currentMatrix) {
+function editFigure(currentFigure, factor) {
     var x = 0, y = 0
     if (currentFigure.cx) {
         x = currentFigure.cx;
         y = currentFigure.cy;
         if (x) {
-            currentFigure.cx = x * currentMatrix['a'] - y * currentMatrix['b'] + currentMatrix['e']
-            currentFigure.cy = -x * currentMatrix['c'] + y * currentMatrix['d'] + currentMatrix['f']
+            currentFigure.cx = x * factor
+            currentFigure.cy = y * factor
+            maxHeight = Math.max(currentFigure.cy, maxHeight)
+            maxWidth = Math.max(currentFigure.cx, maxWidth)
         }
     } else if (currentFigure.x) {
         x = currentFigure.x
         y = currentFigure.y
-        currentFigure.x = x * currentMatrix['a'] - y * currentMatrix['b'] + currentMatrix['e']
-        currentFigure.y = -x * currentMatrix['c'] + y * currentMatrix['d'] + currentMatrix['f']
+        currentFigure.x = x * factor
+        currentFigure.y = y * factor
+        maxHeight = Math.max(currentFigure.y, maxHeight)
+        maxWidth = Math.max(currentFigure.x, maxWidth)
     }
-    currentFigure.transform = editTransform(currentFigure.transform, currentMatrix)
+    currentFigure.transform = editTransform(currentFigure.transform, factor)
     return currentFigure
 }
 
-function editPath(currentPath, currentMatrix) {
+function editPath(currentPath, factor) {
     var k = currentPath.split(' ')
     var doTransfo = false
     var onlyRotation = false;
     return k.map((z) => {
-        if (z.length === 1) {
-            doTransfo = z === 'm'
-        } else if (z.length === 1) {
-            doTransfo = z !== 'm'
-        }
-        if(doTransfo && z.includes(',')) {
+        if (z.includes(',')) {
             var j = z.split(',');
-            if (!onlyRotation) {
-                onlyRotation = true
-                return [
-                    j[0] * currentMatrix['a'] - j[1] * currentMatrix['b'] + currentMatrix['e'],
-                    -j[0] * currentMatrix['c'] + j[1] * currentMatrix['d'] + currentMatrix['f'],
-                ].join(',');
-            }
+            var x = j[0] * factor
+            var y = j[1] * factor
+            maxHeight = Math.max(y, maxHeight)
+            maxWidth = Math.max(x, maxWidth)
             return [
-                j[0] * currentMatrix['a'] - j[1] * currentMatrix['b'],
-                -j[0] * currentMatrix['c'] + j[1] * currentMatrix['d'],
-            ].join(',');
-        } else if (z.includes(',') && k.length > 5 ) {
-            var j = z.split(',');
-            return [
-                j[0]*currentMatrix['a']-j[1]*currentMatrix['b'],
-                -j[0]*currentMatrix['c']+j[1]*currentMatrix['d'],
+                x,
+                y,
             ].join(',');
         }
         return z;
@@ -121,13 +99,13 @@ function editPath(currentPath, currentMatrix) {
 }
 
 async function main() {
-    const svg = await fs.readFile('tmp.svg')
+    const svg = await fs.readFile('src/data/map.svg')
     var parser = new xml2js.Parser(/* options */);
     var svgInXml = await parser.parseStringPromise(svg)
-    var initialMatrix = { 'a': 1, 'b': 0, 'c': 0, 'd': 1, 'e': 0, 'f': 0 }
+    var factor = 2
 
     for (const group of svgInXml.svg.g) {
-        updateSubGroup(group, initialMatrix)
+        updateSubGroup(group, factor)
         group['$'].transform = undefined
     }
 
@@ -139,16 +117,6 @@ async function main() {
         offset: 4,
         newline: ' ',
     }));
+    console.log(`${maxWidth} ${maxHeight}`)
 }
 void main()
-
-function multipliMatrix(m1,m2) {
-    return {
-        'a': m1['a']*m2['a']+m1['c']*m2['b'],
-        'b': m1['b']*m2['a']+m1['d']*m2['b'],
-        'c': m1['a']*m2['c']+m1['c']*m2['d'],
-        'd': m1['b']*m2['c']+m1['d']*m2['d'],
-        'e': m1['a']*m2['e']+m1['c']*m2['e']+m1['e'],
-        'f': m1['b']*m2['f']+m1['d']*m2['f']+m1['f'],
-    }
-}
